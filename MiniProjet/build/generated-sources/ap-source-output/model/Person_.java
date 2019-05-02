90a38db48989909d3f57c249897de2326a65c8f1
oumaymameddeb <oumayma.meddeb.11@gmail.com>
2019-05-02 12:55:07 +0100

    design

--- MiniProjet/build/generated-sources/ap-source-output/model/Person_.java
+++ MiniProjet/build/generated-sources/ap-source-output/model/Person_.java
@@ -4,11 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-01T16:01:55")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-01T01:33:16")
->>>>>>> 37a29dd06ed114abd9e3648c918672024c038571
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-02T12:46:26")
 @StaticMetamodel(Person.class)
 public class Person_ { 
 
